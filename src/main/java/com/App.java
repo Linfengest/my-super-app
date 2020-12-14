5b5ade68562e283b2a0e9c791b85673bf4829489
gyw <157793728@qq.com>
2020-12-14 19:55:01 +0800

    修改启动项文件

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -2,9 +2,7 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController   //返回json字符串数据，来编写RESTful接口
 @SpringBootApplication   //让spring boot自动给程序进行必要的配置（什么必要的配置？）
 public class App {
 
